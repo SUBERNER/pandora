dc0d40dba57a0097b09a3ee64f11af3d0fc459ad
SUBERNER <80803238+SUBERNER@users.noreply.github.com>
2024-11-26 11:41:46 -0600

    Searches
    Updating The customization of searches, allowing users to search if a file or text contains all of the contains or just one of the contains.

--- Buffle/Search/Search.py
+++ Buffle/Search/Search.py
@@ -1,4 +1,8 @@
 import os
+from typing import Literal
+
+# used to determine how the search finds files
+_TYPE_SEARCH = Literal["any", "all"]
 
 
 # region FULL
@@ -18,7 +22,7 @@ def full(source: str, deep_search: bool, inverse_search: bool):
 
 
 # region NAME
-def name(source: str, contains: str | list[str], deep_search: bool, inverse_search: bool):
+def name(source: str, contains: str | list[str], deep_search: bool, inverse_search: bool, type_search: _TYPE_SEARCH = "all"):
     files = []  # will store all files found in this search
 
     # makes contains always a list
@@ -30,14 +34,17 @@ def name(source: str, contains: str | list[str], deep_search: bool, inverse_sear
     if deep_search:  # if enabled, will also go through all subfolders inside source
         folders = [f[0] for f in os.walk(source)]  # get all subfolders inside source
         for folder in folders:  # goes through each folder
-            files.extend([f for f in os.scandir(folder) if f.is_file() and ((all(contain in f.name for contain in contains) and not inverse_search) or (all(contain not in f.name for contain in contains) and inverse_search))])  # gets all files with the substring inside source directory
+            if type_search == "all":
+                files.extend([f for f in os.scandir(folder) if f.is_file() and ((all(contain in f.name for contain in contains) and not inverse_search) or (all(contain not in f.name for contain in contains) and inverse_search))])  # gets all files with the substring inside source directory
+            elif type_search == "any":
+                files.extend([f for f in os.scandir(folder) if f.is_file() and ((any(contain in f.name for contain in contains) and not inverse_search) or (any(contain not in f.name for contain in contains) and inverse_search))])  # gets all files with the substring inside source directory
 
     return files  # returns all files in a list
 # endregion
 
 
 # region CONTENT
-def content(source: str, contains: str | list[str], deep_search: bool, inverse_search: bool):
+def content(source: str, contains: str | list[str], deep_search: bool, inverse_search: bool, type_search: _TYPE_SEARCH = "all"):
     original_files = []  # will store all files found in this search
     formatted_files = []  # will store all the files after determining their content
 
@@ -58,11 +65,11 @@ def content(source: str, contains: str | list[str], deep_search: bool, inverse_s
         if entry.is_file():
             with open(entry, 'r') as file:
                 text = file.read()
-                if not inverse_search:
-                    if all(contain in text for contain in contains):  # determines if contains is inside the file text
+                if type_search == "all":
+                    if ((all(contain in text for contain in contains) and not inverse_search) or (all(contain not in text for contain in contains) and inverse_search)):  # determines if contains is inside the file text
                         formatted_files.append(entry)
-                else:
-                    if all(contain not in text for contain in contains):  # determines if contains is not inside the file text
+                elif type_search == "any":
+                    if ((any(contain in text for contain in contains) and not inverse_search) or (any(contain not in text for contain in contains) and inverse_search)):  # determines if contains is not inside the file text
                         formatted_files.append(entry)
 
     return formatted_files  # returns all files in a list
