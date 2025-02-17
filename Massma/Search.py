3b004c1d516f1086c9e964bc57576e608838b852
SUBERNER <80803238+SUBERNER@users.noreply.github.com>
2025-02-17 11:02:11 -0600

    Moving to laptop
    Working on search currently, still working on it

--- Massma/Search.py
+++ Massma/Search.py
@@ -1,10 +1,31 @@
+from Massma import random  # used for seeds
+from Massma.Filter import *
 import Massma
 import os
 
 
-def full(source: str, *, deep_search: bool = False, inverse_search: bool = False, chance: float = 1) -> list[str]:
+def full(source: str, *, deep_search: bool = False, inverse_search: bool = False, chance_files: float = 1, chance_folders: float = 1,
+         ignores: Ignore | list[Ignore] | None = None, excludes: Exclude | list[Exclude] | None = None, alters: Alter | list[Alter] | None = None) -> list[str]:
     try:
-        pass
+        files = []  # will store all files found in this search
+
+        if not inverse_search:  # ignores everything but the source if an inverse_search
+            files.extend([f.path for f in os.scandir(source) if f.is_file()])  # gets all files inside source directory
+
+        if deep_search:  # if enabled, will also go through all subfolders inside source
+            folders = [f[0] for f in os.walk(source)]  # get all subfolders inside source
+            for folder in folders:  # goes through each folder
+                if chance_folders >= random.random():  # test chances to see if a folder will be seached
+                    if folder == source:  # skips source folder, as it is already scanned in the first if
+                        continue
+                    files.extend([f.path for f in os.scandir(folder) if f.is_file()])  # gets all files inside source directory
+
+        for file in files:   # test chances to see if files will stay in list
+            if chance_files <= random.random():
+                files.remove(file)
+
+        Massma.Display.search.result(source, "full", 0, len(files))
+        return files  # returns all files in a list
     except Exception as e:
         Massma.Display.audio.error_result(source, "full", e)
         return []
@@ -12,7 +33,10 @@ def full(source: str, *, deep_search: bool = False, inverse_search: bool = False
 
 def name(source: str) -> list[str]:
     try:
-        pass
+        files = []  # will store all files found in this search
+
+        Massma.Display.search.result(source, "name", len(files), 0)
+        return files  # returns all files in a list
     except Exception as e:
         Massma.Display.audio.error_result(source, "name", e)
         return []
@@ -20,7 +44,10 @@ def name(source: str) -> list[str]:
 
 def content(source: str) -> list[str]:
     try:
-        pass
+        files = []  # will store all files found in this search
+
+        Massma.Display.search.result(source, "content", len(files), 0)
+        return files  # returns all files in a list
     except Exception as e:
         Massma.Display.audio.error_result(source, "content", e)
         return []
--- NEW_TEST.py
+++ NEW_TEST.py
@@ -3,53 +3,54 @@ import os
 import re
 
 result_test = Massma.Display.Result()
-#try:
+try:
 
-print(F"TESTING, HELLO THERE!")
-for color in Massma.Display.Color.GROUPS:
-    print(f"{color}TESTING, HELLO THERE!{Massma.Display.Color.RESET}")
+    print(F"TESTING, HELLO THERE!")
+    for color in Massma.Display.Color.GROUPS:
+        print(f"{color}TESTING, HELLO THERE!{Massma.Display.Color.RESET}")
 
-result_test.set_source_length("CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd())
+    result_test.set_source_length("CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd())
 
-print(f"{Massma.Display.Color.NOTIFY}TESTING, THIS IS A NOTIFICATION!{Massma.Display.Color.RESET}")
-print(f"{Massma.Display.Color.ERROR}TESTING, THIS IS A ERROR!{Massma.Display.Color.RESET}")
-print(f"{Massma.Display.Color.WARNING}TESTING, THIS IS A WARNING!{Massma.Display.Color.RESET}")
+    print(f"{Massma.Display.Color.NOTIFY}TESTING, THIS IS A NOTIFICATION!{Massma.Display.Color.RESET}")
+    print(f"{Massma.Display.Color.ERROR}TESTING, THIS IS A ERROR!{Massma.Display.Color.RESET}")
+    print(f"{Massma.Display.Color.WARNING}TESTING, THIS IS A WARNING!{Massma.Display.Color.RESET}")
 
-result_test.result("CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "NORMAL", 0, 1)
-result_test.result("CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "NORMAL", 20, 0)
-result_test.result("CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "NORMAL", 5, 5)
-result_test.result_warning("CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "WARN", "this is a warning\nMORE WARNINGS")
-result_test.result_notify("CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "NOTI", "have a good day")
-print("YES DONE")
+    result_test.result("CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "NORMAL", 0, 1)
+    result_test.result("CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "NORMAL", 20, 0)
+    result_test.result("CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "NORMAL", 5, 5)
+    result_test.result_warning("CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "WARN", "this is a warning\nMORE WARNINGS")
+    result_test.result_notify("CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "NOTI", "have a good day")
+    print("YES DONE")
 
-Massma.Display.filter.set_source_length(25)
-Massma.Display.filter.set_source_compression(True)
-swapping = Massma.Filter.Swap([(os.getcwd() + "\\ONE.txt", os.getcwd() + "\\THREE.txt"), (os.getcwd() + "\\TWO.txt", os.getcwd() + "\\FOUR.txt")])
-swapping()
-ignoring = Massma.Filter.Ignore(["CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd()])
-ignoring("CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd())
-ignoring("bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd())
-swapping()
+    Massma.Display.filter.set_source_length(25)
+    Massma.Display.filter.set_source_compression(True)
+    swapping = Massma.Filter.Swap([(os.getcwd() + "\\ONE.txt", os.getcwd() + "\\THREE.txt"), (os.getcwd() + "\\TWO.txt", os.getcwd() + "\\FOUR.txt")])
+    swapping()
+    ignoring = Massma.Filter.Ignore(["CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd(), "bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd()])
+    ignoring("CHANGED\\bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd())
+    ignoring("bedrock-samples-1.21.50.7\\CHANGED\\bedrock-samples-1.21.50.7\\" + os.getcwd())
+    swapping()
 
-print("EXCLUDING")
+    print("EXCLUDING")
 
-exclude = Massma.Filter.Exclude(os.getcwd() + "\\ONE.txt", ["TWO", "SIX"], test_files=[os.getcwd() + "\\TWO.txt", os.getcwd() + "\\SIX.txt"],
-                                logic_files=Massma.Logic.OR, logic_strings=Massma.Logic.OR)
-exclude(os.getcwd() + "\\ONE.txt")
+    exclude = Massma.Filter.Exclude(os.getcwd() + "\\ONE.txt", ["TWO", "SIX"], test_files=[os.getcwd() + "\\TWO.txt", os.getcwd() + "\\SIX.txt"],
+                                    logic_files=Massma.Logic.OR, logic_strings=Massma.Logic.OR)
+    exclude(os.getcwd() + "\\ONE.txt")
 
-print("INPUT")
+    print("INPUT")
 
-inputting = Massma.Filter.Input(os.getcwd() + "\\ONE.txt", ["111", "222", "333", "111"], logic_inputs=Massma.Logic.XNOR)
-inputting(os.getcwd() + "\\ONE.txt", "111")
+    inputting = Massma.Filter.Input(os.getcwd() + "\\ONE.txt", ["111", "222", "333", "111"], logic_inputs=Massma.Logic.XNOR)
+    inputting(os.getcwd() + "\\ONE.txt", "111")
 
-print("ALTER")
-alter_path = os.getcwd() + "\\OTHERS - Copy"
-altering = Massma.Filter.Alter([alter_path + "\\camel.json", alter_path + "\\goat.json", alter_path + "\\sheep.json"]
-                               , [r'"is_spawnable": true', r'"is_summonable": false'], [(r'"entity_type": "minecraft:skeleton"', r'"entity_type": "minecraft:booba"'), (r'"weight": 80', r'"weight": 999')], logic_files=Massma.Logic.OR, logic_strings=Massma.Logic.OR,
-                               replace_files=alter_path + "\\spider.json")
-altering(alter_path + "\\goat.json")
+    print("ALTER")
+    alter_path = os.getcwd() + "\\OTHERS - Copy"
+    altering = Massma.Filter.Alter([alter_path + "\\camel.json", alter_path + "\\goat.json", alter_path + "\\sheep.json"]
+                                   , [r'"is_spawnable": true', r'"is_summonable": false'], [(r'"entity_type": "minecraft:skeleton"', r'"entity_type": "minecraft:booba"'), (r'"weight": 80', r'"weight": 999')], logic_files=Massma.Logic.OR, logic_strings=Massma.Logic.OR,
+                                   replace_files=alter_path + "\\spider.json")
+    altering(alter_path + "\\goat.json")
 
+    Massma.Search.full(os.getcwd() + "\\SEARCH", deep_search=True, chance_files=0.5)
 
-#except Exception as e:
-    #result_test.result_error(os.getcwd(), "TEST", str(e))
-    #print("NO DONE")
+except Exception as e:
+    result_test.result_error(os.getcwd(), "TEST", str(e))
+    print("NO DONE")
