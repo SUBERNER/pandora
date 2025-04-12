1180470fc64b172398447f6561525470b1cf4cde
John Tompkins <80803238+SUBERNER@users.noreply.github.com>
2025-04-11 21:00:27 -0500

    Method templates
    Make a method template that will be expanded on later for the inner and outer methods
    
    Added the duplicate, flatten, weight, preset, and preshuffle that will be expanded later for the methods.
    
    Additionally, renamed multiply to scale and added offset for addition

---
+++ Massma/Inner.py
@@ -0,0 +1,27 @@
+from Massma import random  # used for seeds
+from Massma.Filter import *
+import Massma
+
+def normal(files: str | list[str], contains: str | list[str], *, duplicate: bool = False, flatten: bool = False, weight: list[int] = (), preset: str | list[str] = (), preshuffle: list[int] = (), chance_file: float = 1, chance_total: float = 1, chance_value: float = 1,
+           ignores: Ignore | list[Ignore] | None = None, excludes: Exclude | list[Exclude] | None = None, alters: Alter | list[Alter] | None = None, flags: list[re.RegexFlag] = None):
+    pass
+
+def group(files: str | list[str], contains: str | list[str], *, duplicate: bool = False, flatten: bool = False, weight: list[int] = (), preset: str | list[str] = (), preshuffle: list[int] = (), chance_file: float = 1, chance_total: float = 1, chance_value: float = 1,
+          ignores: Ignore | list[Ignore] | None = None, excludes: Exclude | list[Exclude] | None = None, alters: Alter | list[Alter] | None = None, flags: list[re.RegexFlag] = None):
+    pass
+
+def reverse(files: str | list[str], contains: str | list[str], *, duplicate: bool = False, flatten: bool = False, weight: list[int] = (), preset: str | list[str] = (), preshuffle: list[int] = (), chance_file: float = 1, chance_total: float = 1, chance_value: float = 1,
+            ignores: Ignore | list[Ignore] | None = None, excludes: Exclude | list[Exclude] | None = None, alters: Alter | list[Alter] | None = None, flags: list[re.RegexFlag] = None):
+    pass
+
+def rotate(files: str | list[str], contains: str | list[str], *, duplicate: bool = False, flatten: bool = False, weight: list[int] = (), preset: str | list[str] = (), preshuffle: list[int] = (), chance_file: float = 1, chance_total: float = 1, chance_value: float = 1,
+           ignores: Ignore | list[Ignore] | None = None, excludes: Exclude | list[Exclude] | None = None, alters: Alter | list[Alter] | None = None, flags: list[re.RegexFlag] = None):
+    pass
+
+def scale(files: str | list[str], contains: str | list[str], *, duplicate: bool = False, chance_files: float = 1, chance_total: float = 1,
+          ignores: Ignore | list[Ignore] | None = None, excludes: Exclude | list[Exclude] | None = None, alters: Alter | list[Alter] | None = None, flags: list[re.RegexFlag] = None):
+    pass
+
+def offset(files: str | list[str], contains: str | list[str], *, duplicate: bool = False, chance_files: float = 1, chance_total: float = 1,
+           ignores: Ignore | list[Ignore] | None = None, excludes: Exclude | list[Exclude] | None = None, alters: Alter | list[Alter] | None = None, flags: list[re.RegexFlag] = None):
+    pass
